fcedbce9e9b436938c4419fcd8e892a6cd045de8
vegyanic <125143710+vegyanic@users.noreply.github.com>
2024-09-10 13:48:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/js/bootstrap';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import AboutUs from './pages/aboutus';
-import Newarrival from './pages/Newarrivals';
-import bestseller from './pages/Bestseller';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -59,14 +58,6 @@ const NavRoutes = createBrowserRouter([
   {
     path: 'aboutus',
     element: <AboutUs />,
-  },
-  {
-    path: 'Newarrivals',
-    element: <Newarrival/>,
-  },
-  {
-    path: 'Bestseller',
-    element: <bestseller/>,
   },
   {
     path: '*',  // Fallback route
